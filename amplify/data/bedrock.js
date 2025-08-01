158c53d781ca042a8fbe33c6cab3c7cef036c744
HPN03 <heather.paterson@sky.uk>
2025-08-01 15:05:25 +0100

    fix bedrock body request

--- amplify/data/bedrock.js
+++ amplify/data/bedrock.js
@@ -13,7 +13,6 @@ export function request(ctx) {
           "Content-Type": "application/json",
         },
         body: JSON.stringify({
-          anthropic_version: "bedrock-2023-05-31",
           max_tokens: 1000,
           messages: [
             {
